2cd710785ec6e950e384913d0ff48a2657e7efb6
Corey Kohler <kohler.nai@gmail.com>
2021-04-22 10:08:18 -0600

    added more tests to firsttest

--- cypress/integration/mytests/Test1.js
+++ cypress/integration/mytests/Test1.js
@@ -1,5 +1,18 @@
+/// <reference types="Cypress"/>
+
 describe("My first test", function(){
     it("First test case", function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get(".search-keyword").type("ca")
+        cy.wait(2000)
+        cy.get(".product:visible").should('have.length', 4)
+        cy.get('.products').find(".product").eq(2).contains("ADD TO CART").click()
+        cy.get('.products').find('.product').each(($el,index, $list)=>{
+            const textVeg=$el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews')){
+                $el.find('button').click()
+            }
+        })
     })
 })
+
